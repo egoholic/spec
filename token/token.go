947e9d5617ef5351fd12aa93e939437c2dca9302
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2019-04-17 11:51:07 +0300

    adds more tokens for tokenizer

--- token/token.go
+++ token/token.go
@@ -12,12 +12,14 @@ var (
 	OpeningCurlyBracketToken = NewRuneToken('{')
 	ClosingCurlyBracketToken = NewRuneToken('}')
 	SpaceToken               = NewVariantToken([]Token{NewRuneToken(' '), NewRuneToken('\n'), NewRuneToken('\t'), NewRuneToken('\t')})
-
-	StringToken           = NewTokenListFromString("string")
-	IntToken              = NewTokenListFromString("int")
-	FloatToken            = NewTokenListFromString("float")
-	BoolToken             = NewTokenListFromString("bool")
-	AnyPrimitiveTypeToken = NewVariantToken([]Token{StringToken, IntToken, FloatToken, BoolToken})
+	LowLatinLetterToken      = NewVariantTokenFromRunes([]rune("abcdefghijklmnopqrstuvwxyz"))
+	HighLatinLetterToken     = NewVariantTokenFromRunes([]rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+	DigitToken               = NewVariantTokenFromRunes([]rune("0123456789"))
+	StringToken              = NewTokenListFromString("string")
+	IntToken                 = NewTokenListFromString("int")
+	FloatToken               = NewTokenListFromString("float")
+	BoolToken                = NewTokenListFromString("bool")
+	AnyPrimitiveTypeToken    = NewVariantToken([]Token{StringToken, IntToken, FloatToken, BoolToken})
 
 	AnyStandardTypeToken *VariantToken
 
@@ -34,16 +36,50 @@ type Token interface {
 	Matches(*rawsig.RawSignature) bool
 }
 
-type VariantToken []Token
+type ExceptToken struct {
+	token Token
+}
+
+func NewExceptToken(token Token) *ExceptToken {
+	return &ExceptToken{token}
+}
+
+func (token *ExceptToken) Matches(rawSig *rawsig.RawSignature) bool {
+	return !token.token.Matches(rawSig)
+}
+
+type VariantToken struct {
+	variants []Token
+}
 
 func NewVariantToken(tokens []Token) *VariantToken {
-	vt := VariantToken(tokens)
-	return &vt
+	return &VariantToken{tokens}
+}
+
+func NewVariantTokenFromRunes(runes []rune) *VariantToken {
+	var tokens []Token
+	for _, r := range runes {
+		tokens = append(tokens, NewRuneToken(r))
+	}
+
+	return &VariantToken{tokens}
+}
+
+func (token *VariantToken) Join(token2 *VariantToken) *VariantToken {
+	var joined = make([]Token, 10)
+	for _, t := range token.variants {
+		joined = append(joined, t)
+	}
+
+	for _, t := range token2.variants {
+		joined = append(joined, t)
+	}
+
+	return &VariantToken{joined}
 }
 
 func (token *VariantToken) Matches(rawSig *rawsig.RawSignature) bool {
-	tokens := []Token(*token)
-	for _, token := range tokens {
+	for _, token := range token.variants {
 		if token.Matches(rawSig) {
 			return true
 		}
@@ -57,23 +93,76 @@ type KeyValuePairToken struct {
 	val Token
 }
 
-func NewKeyValuePairToken() *KeyValuePairToken {
-
+func NewKeyValuePairToken(key, sep, val Token) *KeyValuePairToken {
+	return &KeyValuePairToken{key, sep, val}
 }
 
-type RuneToken rune
+func (token *KeyValuePairToken) Matches(rawSig *rawsig.RawSignature) bool {
+	return token.key.Matches(rawSig) && token.sep.Matches(rawSig) && token.val.Matches(rawSig)
+}
+
+func (token *KeyValuePairToken) MatchesWithoutKey(rawSig *rawsig.RawSignature) bool {
+	return token.sep.Matches(rawSig) && token.val.Matches(rawSig)
+}
+
+type KeyValuePairsSetToken struct {
+	pairs map[Token]*KeyValuePairToken
+}
+
+func NewKeyValuePairsSetToken(pairs map[Token]*KeyValuePairToken) *KeyValuePairsSetToken {
+	return &KeyValuePairsSetToken{pairs}
+}
+
+// This naive implementation assumes that key-value pairs are ordered and order matters during comparing.
+// So that:
+//     a:1, b:2 and b:2, a:1 are different tokens.
+// Also this implementation does not support subset match.
+// So that:
+//     a:1 and a:1, b:2 could not match.
+//
+func (token *KeyValuePairsSetToken) Matches(rawSig *rawsig.RawSignature) bool {
+	for _, pairToken := range token.pairs {
+		if !pairToken.Matches(rawSig) {
+			return false
+		}
+	}
+	return true
+}
+
+type AnyRuneToken struct{}
+
+func NewAnyRuneToken() *AnyRuneToken {
+	return &AnyRuneToken{}
+}
+
+func (token *AnyRuneToken) Matches(rawSig *rawsig.RawSignature) bool {
+	_, err := rawSig.Next()
+	return err == nil
+}
+
+type RuneToken struct {
+	r rune
+}
 
 func NewRuneToken(r rune) *RuneToken {
-	t := RuneToken(r)
-	return &t
+	return &RuneToken{r}
 }
 
 func (token *RuneToken) Matches(rawSig *rawsig.RawSignature) bool {
-	runeT, err := rawSig.Next()
+	r, err := rawSig.Next()
 	if err != nil {
 		return false
 	}
-	return runeT == rune(*token)
+	return r == token.r
+}
+
+type WordToken struct {
+	tokenList *TokenList
+}
+
+func NewWordToken(word string) *WordToken {
+	runes := []rune(word)
+
 }
 
 type TokenList struct {
